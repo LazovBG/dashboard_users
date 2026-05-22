6af23b245599841c9361f19407e0aa5261407caf
LazovBG <37182609+LazovBG@users.noreply.github.com>
2026-05-22 09:34:33 +0300

    Update welcome message and exit button layout

--- new_app.py
+++ new_app.py
@@ -86,10 +86,10 @@ if not st.session_state.user_id:
       register_user(username, password, email)
       st.success("Регистрацията е успешна!")
 else:
-  st.header(f"Добре дошъл, {st.session_state.username}")
-  #print(st.session_state)
+  
   
   col1, col2 = st.columns([4, 1])
+  col1.header(f"Добре дошъл, {st.session_state.username} 👋")
   col1.button("Изход") # бутонът за изход вдясно
   if col2.button("Изход"):
     st.session_state.user_id = None
