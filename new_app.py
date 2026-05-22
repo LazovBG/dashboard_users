28e96fcc0ffbc61d58f39761d7df9fa3a7278340
LazovBG <37182609+LazovBG@users.noreply.github.com>
2026-05-22 17:33:57 +0300

    Implement user and task management functions

--- new_app.py
+++ new_app.py
@@ -4,6 +4,7 @@ import sqlite3
 import bcrypt
 
 # ===== DATABASE =====
+#добавяне на таблицата за потребители
 def init_db():
   conn = sqlite3.connect("lazov_tools.db")
   cursor = conn.cursor()
@@ -15,7 +16,7 @@ def init_db():
       email TEXT 
     )
   """)
-
+  #добавяне на таблицата за задачи
   cursor.execute("""
       CREATE TABLE IF NOT EXISTS tasks (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -53,6 +54,48 @@ if "user_id" not in st.session_state:
 if "username" not in st.session_state:
     st.session_state.username = None
 
+#Тук трябва да добавя кода за добавяне на задача но на определен потребител
+#def get_tasks(user_id): ...
+def get_tasks(user_id):
+  conn = sqlite3.connect("lazov_tools.db")
+  cursor = conn.cursor()
+  cursor.execute("SELECT * FROM tasks WHERE id = ?", (user_id,))
+  rows = cursor.fetchall()
+  conn.close()
+  return rows
+
+#добавяне на потребителя
+#def register_user(username, password, email
+def register_user(username, password, email):
+  conn = sqlite3.connect("lazov_tools.db")
+  cursor = conn.cursor()
+  hashed_passwoed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+  cursor.execute("INSERT INTO users (username, pasword, email) VALUES (?, ?, ?)", (username, hashed_passwoed, email))
+  conn.commit()
+  conn.close()
+
+#тярбва да добавя добавяне на задача
+#def add_task(title, user_id): ...
+def add_task(title, user_id):
+  conn = sqlite3.connect("lazov_tools.db")
+  cuesor = conn.cursor()
+  cursor.execute("INSERT INTO tasks (title, user_id) VALUES (?, ?)", (title, user_id))
+  conn.commit()
+  conn.close()
+
+#да се добави извършване на задача на определен потребител
+#def complete_task(task_id): ...
+def complete_task(task_id):
+  conn = sqlite3.connect("lazov_tools.db")
+  cursor = conn.cursor()
+  cursor.execute("UPDATE tasks SET done = 1 WHERE id = ?", (task_id,))
+  conn.commit()
+  conn.close()
+
+#Да се добави изтриване назадача на потребител
+#def delete_task(task_id): ...
+
+
 # UI частта
 init_db()
 st.title("🗂️ Lazov Tools")
@@ -85,7 +128,7 @@ if not st.session_state.user_id:
     if st.button("Регистрация"):
       register_user(username, password, email)
       st.success("Регистрацията е успешна!")
-else:  
+else:
   col1, col2 = st.columns([4, 1])
   col1.header(f"Добре дошъл, {st.session_state.username} 👋")
   if col2.button("Изход"):
